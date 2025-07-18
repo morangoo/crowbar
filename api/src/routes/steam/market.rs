fdd82cdb9fc35d496149e07399a475301dd42174
Bruno Santos <bms.santos@outlook.pt>
2025-07-18 13:55:25 +0100

    GET Top 1 market item + title

--- api/src/routes/steam/market.rs
+++ api/src/routes/steam/market.rs
@@ -1,13 +1,38 @@
 use rocket::get;
 use rocket::response::content::RawHtml;
 use reqwest;
+use scraper::{Html, Selector};
 
-#[get("/market/ping")]
+#[get("/")]
 pub async fn ping() -> RawHtml<String> {
-    // Fetch the HTML from the Steam Market page
-    match reqwest::get("https://steamcommunity.com/market/").await {
+    // Fetch the HTML from the Steam Market page with Accept-Language header
+    let client = reqwest::Client::new();
+    let request = client
+        .get("https://steamcommunity.com/market/");
+        //.header("Accept-Language", "pt-PT,pt;q=0.7");
+    match request.send().await {
         Ok(resp) => match resp.text().await {
-            Ok(html) => RawHtml(html),
+            Ok(html) => {
+                let document = Html::parse_document(&html);
+                let img_selector = Selector::parse("img#result_0_image").unwrap();
+                let name_selector = Selector::parse("span#result_0_name").unwrap();
+                let img_html = if let Some(img) = document.select(&img_selector).next() {
+                    if let Some(src) = img.value().attr("src") {
+                        format!("<img src=\"{}\" />", src)
+                    } else {
+                        "<h1>Image not found</h1>".to_string()
+                    }
+                } else {
+                    "<h1>Image not found</h1>".to_string()
+                };
+                let name_html = if let Some(name) = document.select(&name_selector).next() {
+                    let name_text = name.text().collect::<Vec<_>>().join("").trim().to_string();
+                    format!("<div><b>{}</b></div>", name_text)
+                } else {
+                    "<div><b>Name not found</b></div>".to_string()
+                };
+                RawHtml(format!("{}{}", name_html, img_html))
+            },
             Err(_) => RawHtml("<h1>Error reading Steam Market response</h1>".to_string()),
         },
         Err(_) => RawHtml("<h1>Error fetching Steam Market</h1>".to_string()),
