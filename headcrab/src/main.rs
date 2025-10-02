9663a00ffa80836fdd70986cc62e328eca5352f6
Bruno Santos <bms.santos@outlook.pt>
2025-10-02 16:50:30 +0100

    [testing][hdcrb] testing looping & inserting each app

--- headcrab/src/main.rs
+++ headcrab/src/main.rs
@@ -1,8 +1,9 @@
 use std::{thread, time::Duration};
-use reqwest::Error;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use mongodb::{Client, options::ClientOptions, bson::doc};
+use reqwest;
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct App {
     appid: u32,
     name: String,
@@ -19,18 +20,25 @@ struct ApiResponse {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let client_uri = "mongodb://localhost:27017";
+    let client_options = ClientOptions::parse(client_uri).await?;
+    let client = Client::with_options(client_options)?;
+    let db = client.database("crate");
+    let collection = db.collection::<App>("apps");
+
     loop {
         let resp = reqwest::get("https://api.steampowered.com/ISteamApps/GetAppList/v2/")
             .await?
             .json::<ApiResponse>()
             .await?;
 
-        for app in resp.applist.apps.iter() {
-            println!("AppID: {}, Nome: {}", app.appid, app.name);
+        for app in &resp.applist.apps {
+            collection.insert_one(app, None).await?;
+            tokio::time::sleep(std::time::Duration::from_secs(2)).await; // 2 seconds
         }
 
-        println!("Tarefa finalizada. Aguardando 4 horas para reiniciar...");
-        thread::sleep(Duration::from_secs(4 * 60 * 60)); // 4 horas
+        thread::sleep(Duration::from_secs(4 * 60 * 60)); // 4 hours
     }
 }
